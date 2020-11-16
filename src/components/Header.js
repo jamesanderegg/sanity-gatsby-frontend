34b416a5d779f62fea52dc904bf5b5bdf0732c70
Juicyjames <iceandjames@gmail.com>
2020-11-16 13:00:23 -0700

    testing

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ const HeaderComponent = props => {
   // run graphql queries, setState, useContext, etc.
   return (
     <Header>
-      <h1>Edit pages/index.js</h1>
+      <h1>Edit pages/index.js HELLO WORLD</h1>
     </Header>
   )
 }
